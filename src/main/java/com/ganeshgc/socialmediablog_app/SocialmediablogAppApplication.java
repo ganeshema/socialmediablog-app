f5a9b63f3ca82fa0f987bd49d6356d873c01c1bc
Ganesh GC <ganeshlema2021@gmail.com>
2024-06-21 22:29:05 -0500

    First Commit of social media blog app templete project

---
+++ src/main/java/com/ganeshgc/socialmediablog_app/SocialmediablogAppApplication.java
@@ -0,0 +1,13 @@
+package com.ganeshgc.socialmediablog_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediablogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediablogAppApplication.class, args);
+	}
+
+}
