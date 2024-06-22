b427ff0e8f47ea96351e83f2fbbc56a94a67bb9f
Ganesh GC <ganeshlema2021@gmail.com>
2024-06-21 22:58:43 -0500

    initial project setup files for social media blog app for main branch

---
+++ src/test/java/com/ganeshgc/socialmediablog_app/SocialmediablogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshgc.socialmediablog_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediablogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
